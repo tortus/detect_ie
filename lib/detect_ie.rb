69b9bfc59de5c1a41808424e23acad7ee92c6239
William Makley <wmakley@tortus.com>
2015-01-16 12:58:47 -0500

    add specs for basic functionality

--- lib/detect_ie.rb
+++ lib/detect_ie.rb
@@ -6,7 +6,7 @@ module DetectIE
   # Return version as integer for all IE versions less than 10, returns 10 otherwise since we
   # generally treat 10 the same as Chrome or Firefox.
   def ie_version
-    if @force_ie_version.present?
+    if @force_ie_version
       @force_ie_version
     elsif request.env['HTTP_USER_AGENT'] =~ /MSIE (\d+)\.0/
       $1.to_i
@@ -17,7 +17,7 @@ module DetectIE
 
   # Add to template or controller before IE version-dependent code to aid in testing.
   def force_ie_version!(version)
-    if Rails.env.production?
+    if defined?(Rails) && Rails.env.production?
       logger.warn "IE version should not be forced in production in environment! Please remove any calls to 'force_ie_version!' before release."
     end
     @force_ie_version = version.to_i
--- spec/lib/detect_ie/detect_ie_spec.rb
+++ spec/lib/detect_ie/detect_ie_spec.rb
@@ -1,5 +1,63 @@
 require 'spec_helper'
 
 describe DetectIE do
-  pending
+
+  IE_8_USER_AGENT_STRING = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)"
+
+  let(:user_agent) { IE_8_USER_AGENT_STRING }
+
+  let(:mock_controller) do
+    Class.new do
+      include DetectIE
+    end
+  end
+
+  subject do
+    controller = mock_controller.new
+    controller.stub(:request) do
+      double("request").tap do |rq|
+        rq.stub(:env) { {'HTTP_USER_AGENT' => user_agent} }
+      end
+    end
+    controller
+  end
+
+  def ie_8_user_agent_string
+  end
+
+  context "when included" do
+    it "calls helper_method for all DetectIE methods" do
+      controller_cls = Class.new
+      controller_cls.stub(:helper_method) { nil }
+      controller_cls.send(:include, DetectIE)
+      expect(controller_cls).to have_received(:helper_method).with(:ie_version, :force_ie_version!, :ie_8_or_less?, :force_ie_8!)
+    end
+    it "doesn't call helper_method if base class not respond to it" do
+      controller_cls = Class.new
+      expect {
+        controller_cls.send(:include, DetectIE)
+      }.not_to raise_error(NoMethodError)
+    end
+  end
+
+  describe "#ie_version" do
+    it "returns 8 when user agent string is IE 8" do
+      expect(subject.send(:ie_version)).to eq(8)
+    end
+    context "when user agent string doesn't look like IE at all" do
+      let(:user_agent) { "ANYTHING" }
+      it "returns 10" do
+        expect(subject.send(:ie_version)).to eq(10)
+      end
+    end
+    context "when #force_ie_version! has been called" do
+      before do
+        subject.send(:force_ie_version!, 7)
+      end
+
+      it "returns the forced IE version" do
+        expect(subject.send(:ie_version)).to eq(7)
+      end
+    end
+  end
 end
